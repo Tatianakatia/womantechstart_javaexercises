4cb8c54685d637cda459e534054077c7db4cc254
unknown <tatiana.katia@gmail.com>
2022-04-30 13:53:48 +0000

    exercises HelloWord and VariablesExercise

---
+++ VariablesExercise/Main.java
@@ -0,0 +1,40 @@
+// *****Program: "Junior Software Developer Accelerate Program" *****
+//      Version1 TechStart program
+//      SkillNett Women Tech Start
+// Training:
+//      WorkJuggle and C2 : Tecnical and Professional Skills program
+//
+// Class: Introduction to Java
+// Instructor: Martina Yusuf
+// Developer: Tatiana Katia❤⛵
+
+public class Main {
+    public static void main(String[] args) {
+//        // Task 1 String Variable
+//        String firstName;
+//        String lastName;
+//        firstName = "Tatiana";
+//        lastName = "Silva";
+//        String name = firstName + " " + lastName;
+//        System.out.println( name);
+
+//        int num1 = 5, num2 = 2;
+//        int sum = num1 + num2;
+//        System.out.println(sum);
+//
+//        float result = num1 / num2;
+//        System.out.println(result);
+//
+//        int num3;
+//        int num3++;
+
+
+
+
+
+
+
+
+
+    }
+}
